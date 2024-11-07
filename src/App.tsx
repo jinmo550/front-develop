cf6546c85984bcb8d66826389d654da37b59a202
SeungHyun CHA <cjhms002@g.yju.ac.kr>
2024-11-07 22:52:27 +0900

    ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import BulletinBoard from "./pages/BulletinBoard";
+import Introduction from "./pages/Introduction";
+import LocalSemester from "./pages/LocalSemester";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/bulletinBoard" element={<BulletinBoard />} />
+          <Route path="/introduction" element={<Introduction />} />
+          <Route path="/localSemester" element={<LocalSemester />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+// 홈 페이지
+import "../style/home.css";
+
+const Home = () => {
+  return <div>Home</div>;
+};
+
+export default Home;
