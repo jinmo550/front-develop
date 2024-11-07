83327da87aae7f0a354f173e2d5a422ae8adc7db
SeungHyun CHA <cjhms002@g.yju.ac.kr>
2024-11-07 23:00:17 +0900

    wow

--- src/pages/BulletinBoard.tsx
+++ src/pages/BulletinBoard.tsx
@@ -1,8 +1,36 @@
 // 게시판 페이지
 import "../styles/bulletinboard.css";
+import { useNavigate } from "react-router-dom";
 
 const BulletinBoard = () => {
-  return <div>BulletinBoard</div>;
+  const navigate = useNavigate();
+
+  const onClick0 = () => {
+    navigate("/home");
+  };
+
+  const onClick1 = () => {
+    navigate("/introduction");
+  };
+
+  const onClick2 = () => {
+    navigate("/localSemester");
+  };
+
+  const onClick3 = () => {
+    navigate("/bulletinBoard");
+  };
+  return (
+    <div className="bulletin-board">
+      BulletinBoard
+      <div className="container">
+        <button onClick={onClick0}>홈</button>
+        <button onClick={onClick1}>조원소개</button>
+        <button onClick={onClick2}>현지학기</button>
+        <button onClick={onClick3}>게시판</button>
+      </div>
+    </div>
+  );
 };
 
 export default BulletinBoard;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,37 @@
 // 홈 페이지
 import "../styles/home.css";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  const navigate = useNavigate();
+
+  const onClick0 = () => {
+    navigate("/home");
+  };
+
+  const onClick1 = () => {
+    navigate("/introduction");
+  };
+
+  const onClick2 = () => {
+    navigate("/localSemester");
+  };
+
+  const onClick3 = () => {
+    navigate("/bulletinBoard");
+  };
+
+  return (
+    <div className="home">
+      Home
+      <div className="container">
+        <button onClick={onClick0}>홈</button>
+        <button onClick={onClick1}>조원소개</button>
+        <button onClick={onClick2}>현지학기</button>
+        <button onClick={onClick3}>게시판</button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
